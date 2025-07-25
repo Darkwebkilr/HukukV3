2d06d83574b2af74265b0c48094eb707fd9b2794
Darkwebkilr <gbaziz560@gmail.com>
2025-07-25 03:47:26 +0300

    feat: Add Astro Cloudflare adapter for SSR deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.enessevinc.com',
   integrations: [tailwind(), sitemap()],
+  adapter: cloudflare(),
 });
